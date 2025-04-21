844cfc30485a9bc91a6cbb1c585eab076f4628b7
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-21 17:18:22 +0530

    define RootStackParamList and navigation type for Home screen

---
+++ src/src/types/navigation.ts
@@ -0,0 +1,12 @@
+// src/types/navigation.ts
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+  Home: undefined;
+  AddTask: undefined;
+};
+
+export type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
