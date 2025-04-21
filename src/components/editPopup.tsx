5dc845c68a1d559644c134b311f0491d063714b8
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-21 11:14:07 +0530

    Developed the separate component for task editing

---
+++ src/components/editPopup.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import styles from '../styles/componentStyles/editPopup.style';
+
+type Props = {
+  visible: boolean;
+  initialTitle: string;
+  initialAbout: string;
+  onCancel: () => void;
+  onSave: (title: string, about: string) => void;
+};
+
+const EditTaskPopup: React.FC<Props> = ({
+    visible,
+    initialTitle,
+    initialAbout,
+    onCancel,
+    onSave,
+  }) => {
+  
+    const [title, setTitle] = useState(initialTitle);
+    const [about, setAbout] = useState(initialAbout);
+
+  useEffect(() => {
+    setTitle(initialTitle);
+    setAbout(initialAbout);
+  }, [initialTitle, initialAbout, visible]);
+
+  if (!visible) return null;
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Title"
+        placeholderTextColor="#A66CFF"
+        value={title}
+        onChangeText={setTitle}
+      />
+
+      <TextInput
+        style={[styles.input, { height: 120, textAlignVertical: 'top' }]}
+        placeholder="Description..."
+        placeholderTextColor="#A66CFF"
+        multiline
+        value={about}
+        onChangeText={setAbout}
+      />
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity onPress={onCancel} style={styles.button}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onSave(title, about)} style={styles.button}>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default EditTaskPopup;
