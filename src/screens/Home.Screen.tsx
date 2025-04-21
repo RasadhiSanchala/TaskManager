0a1595e817f4ae060b0fe6dd13292b1b0ccdc74f
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-21 17:16:22 +0530

    Added navigation to AddTaskScreen

--- src/screens/Home.Screen.tsx
+++ src/screens/Home.Screen.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from '../styles/screenStyles/home.styles'; 
+import { useNavigation } from '@react-navigation/native';
+import { HomeScreenNavigationProp } from '../types/navigation'; 
+import styles from '../styles/screenStyles/home.styles';
 
 const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>(); 
+
+  const handleGetStarted = () => {
+    navigation.navigate('AddTask'); 
+  };
+
   return (
     <View style={styles.container}>
       <Image source={require('../assets/List.png')} style={styles.image} />
       <Text style={styles.title}>Get things done with</Text>
       <Text style={styles.appName}>ToDo</Text>
-      <Text style={styles.subText}>Stay organized and focused with simple task management</Text>
-      
+      <Text style={styles.subText}>
+        Stay organized and focused with simple task management
+      </Text>
+
       {/* Button */}
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleGetStarted}>
         <Text style={styles.buttonText}>Get Started</Text>
       </TouchableOpacity>
     </View>
