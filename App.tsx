7a9e895155592d4f000beb3a738b806c78cadab3
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-21 17:13:46 +0530

    Added navigation feature

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/Home.Screen';
-import AddTaskScreen from './src/screens/AddTask.Screen'
+import AddTaskScreen from './src/screens/AddTask.Screen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <AddTaskScreen/>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="AddTask"
+          component={AddTaskScreen}
+          options={{title: 'Tasks', headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
